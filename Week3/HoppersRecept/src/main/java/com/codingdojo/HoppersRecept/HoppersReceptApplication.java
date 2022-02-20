52628a927bfdbe16aed5db3a61c8c7c00017e5dc
DHerman42 <dherman42@gmail.com>
2022-02-20 09:07:50 -0500

    Hopper's Recept commit

---
+++ Week3/HoppersRecept/src/main/java/com/codingdojo/HoppersRecept/HoppersReceptApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.HoppersRecept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppersReceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersReceptApplication.class, args);
+	}
+
+}
---
+++ Week3/HoppersRecept/src/test/java/com/codingdojo/HoppersRecept/HoppersReceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.HoppersRecept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersReceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
